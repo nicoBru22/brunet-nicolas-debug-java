5df7797bfb9cb016c1fd638584c9b768631bfc9b
nicoBru22 <brunet.nicolas35@gmail.com>
2024-09-03 19:43:01 +0200

    suppression dans anciens commentaires puis ajout de commentaires en francais sur le fichier analyticsCounter. Modification de la terminologie : au lieu de rash, était noté rush à la ligne 30

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,35 +5,40 @@ import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
+
+		//lecture du document "symptom.txt" 
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
+		int i = 0;
+		int headCount = 0;
 		while (line != null) {
-			i++;	// increment i
+			i++;
 			System.out.println("symptom from file: " + line);
+			
+			//compte le nombre de fois où le symptome "headache" est présent
 			if (line.equals("headache")) {
 				headCount++;
 				System.out.println("number of headaches: " + headCount);
 			}
-			else if (line.equals("rush")) {
+			//compte le nombre de fois où le symptome "rash" est présent
+			else if (line.equals("rash")) {
 				rashCount++;
 			}
+			//compte le nombre de fois où le symptome "pupils" est présent
 			else if (line.contains("pupils")) {
 				pupilCount++;
 			}
-
-			line = reader.readLine();	// get another symptom
+			
+			line = reader.readLine();
 		}
 		
-		// next generate output
+		//Ecriture du résultat dans le fichier "result.out"
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
