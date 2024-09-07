3be608621eaf0668bf7bfb8090dec4e0117a2482
nicoBru22 <brunet.nicolas35@gmail.com>
2024-09-07 20:19:37 +0200

    refactorisation de la class AnalyticsCounter en créant une méthode pour compter symptomCount() et une méthode pour trier sortedSymptom(). Code fonctionnel

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -18,22 +18,30 @@ public class AnalyticsCounter {
 		this.monWriter = writer;
 	}
 	
-	public void analyseEtEcritureSymptome() {
-		
-	    List<String> symptoms = monReader.GetSymptoms();
-
+	public Map<String, Integer> symptomCount(List<String> symptoms) {
 	    Map<String, Integer> symptomCounts = new HashMap<>();
 	    for (String symptom : symptoms) {
 	        symptomCounts.put(symptom, symptomCounts.getOrDefault(symptom, 0) + 1);
 	    }
-
+	    return symptomCounts;
+	}
+	
+	public List<String> sortedSymptoms(Map<String, Integer> symptomCounts){
 	    List<Map.Entry<String, Integer>> sortedSymptomEntries = new ArrayList<>(symptomCounts.entrySet());
-
 	    Collections.sort(sortedSymptomEntries, Map.Entry.comparingByKey());
-
 	    List<String> symptomData = sortedSymptomEntries.stream()
 	            .map(entry -> entry.getKey() + ": " + entry.getValue())
 	            .collect(Collectors.toList());
+		return symptomData;
+	}
+	
+	public void analyseEtEcritureSymptome() {
+		
+	    List<String> symptoms = monReader.GetSymptoms();
+	    
+	    Map<String, Integer> symptomCounts = symptomCount(symptoms);
+	    
+	    List<String> symptomData = sortedSymptoms(symptomCounts);
 
 	    monWriter.WriteSymptomData(symptomData);
 	}
